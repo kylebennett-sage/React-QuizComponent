da8d99bc5a91eb9d746e21887316da2cced716dd
Kyle Bennett <kyle.bennett@sage.com>
2021-09-24 17:08:57 +0100

    Module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,7 +18,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd
-          ? <QuizEnd />
+          ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
           : <QuizQuestion
               quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
               showNextQuestionHandler={this.showNextQuestion.bind(this)}
@@ -30,6 +30,10 @@ class Quiz extends Component {
 
   showNextQuestion() {
     this.setState({quiz_position: this.state.quiz_position + 1})
+  }
+
+  handleResetClick() {
+    this.setState({quiz_position: 1})
   }
 }
 
