cca77a89f96fa951cf36b5852753af6234947447
Kyle Bennett <kyle.bennett@sage.com>
2021-09-24 16:54:01 +0100

    Module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,11 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {incorrectAnswer: false}
+  }
+
   render() {
     const answers = this.props.quiz_question.answer_options.map((answer_option, index) =>
       <QuizQuestionButton key={index} button_text ={answer_option} clickHandler={this.handleClick.bind(this)} />
@@ -18,13 +23,17 @@ class QuizQuestion extends Component {
             {answers}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Wrong!</p> : null}
       </main>
     );
   }
 
   handleClick(button_text) {
     if(button_text === this.props.quiz_question.answer){
+      this.setState({incorrectAnswer: false})
       this.props.showNextQuestionHandler()
+    } else {
+      this.setState({incorrectAnswer: true})
     }
   }
 }
